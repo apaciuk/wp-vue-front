8611a602bed056f72eea81a73606c0221d81ecab
Paul Anthony McGowan <xhostcom@gmail.com>
2021-07-31 18:51:56 +0100

    First Commit

---
+++ nuxt.config.js
@@ -0,0 +1,65 @@
+export default {
+  // Target: https://go.nuxtjs.dev/config-target
+  target: 'static',
+  server: {
+    port: 8080 // default: 3000
+  },
+
+  // Global page headers: https://go.nuxtjs.dev/config-head
+  head: {
+    title: 'wp-vue-front',
+    htmlAttrs: {
+      lang: 'en'
+    },
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: '' },
+      { name: 'format-detection', content: 'telephone=no' }
+    ],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+    ]
+  },
+
+  // Global CSS: https://go.nuxtjs.dev/config-css
+  css: [
+    'tachyons/css/tachyons.css'
+  ],
+
+  // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
+  plugins: [
+ 
+  ],
+
+  // Auto import components: https://go.nuxtjs.dev/config-components
+  components: true,
+
+  // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
+  buildModules: [
+  ],
+  // Modules: https://go.nuxtjs.dev/config-modules
+  modules: [
+  '@nuxt/http'
+  ],
+  http: {
+    baseURL: 'https://xhostcom.site.strattic.io/wp-json/wp/v2/', // Used as fallback if no runtime config is provided
+    proxy: false,
+    mode: 'no-cors' // no-cors,
+   },
+   publicRuntimeConfig: {
+    http: {
+      browserBaseURL: process.env.BROWSER_BASE_URL
+    }
+  },
+
+  privateRuntimeConfig: {
+    http: {
+      baseURL: process.env.BASE_URL
+    }
+  },
+
+  // Build Configuration: https://go.nuxtjs.dev/config-build
+  build: {
+  }
+}
---
+++ pages/index.vue
@@ -0,0 +1,27 @@
+<template>
+<div>
+<h1>Index</h1>
+</div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      posts: []
+    }
+  },
+  created: function() {
+     // GET /someUrl
+    this.posts = this.$http.$get('posts').then(response => {
+     console.log(response);
+    // get body data
+    //this.someData = response.body;
+
+  }, response => {
+    // error callback
+  });
+  }
+}
+
+</script>
